6a1f27cc8c07fb254d9c49746d8ebe5b86515c62
Joyce Chin <joycechin0@gmail.com>
2024-07-07 20:28:11 +1000

    Updated config

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+   baseurl: 'https://demo.playwright.dev/todomvc',
+   viewportHeight: 550,
+   viewportWidth: 660,
+   experimentalStudio: true
   },
 });
